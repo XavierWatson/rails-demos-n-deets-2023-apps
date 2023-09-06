2108d1d990be9d9bfb74f2b2500bc10f74ff9642
XavierWatson <143544698+XavierWatson@users.noreply.github.com>
2023-09-06 16:41:19 -0500

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-get 'home', to: 'pages#home', as: 'home'
   # Defines the root path route ("/")
   # root "articles#index"
+
+  root to: redirect('/home')
+
+  get 'home', to: 'pages#home', as: 'home'
+  get 'about', to: 'pages#about', as: 'about'
+
 end
